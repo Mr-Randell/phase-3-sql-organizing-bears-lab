375a1344137700c5af653d19e70832d0daa60117
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-11-01 04:11:56 +0300

    Feat: creating bears table & populating the bears table fully completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 8, "M", "dark brown", "Calm", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ("Rowdy", 16, "M", "Black", "Playfull", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ("Tabitha", 4, "F", "Dark Brown", "Slimy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ("Sargeant Brown", 25, "M", "Green", "Nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ("Melissa", 14, "F", "Dark Brown", "Goofy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ("Grinch", 1, "M", "Black", "Grinchy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ("Wendy", 28, "F", "Black", "Naive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES (NULL, 2, "M", "Blue", "Aggressive", 0);
